576c85eca29a2cf38486c094096d284d00920872
prakash-618 <prakashranjan45890@gmail.com>
2024-08-17 09:10:19 +0000

    commit

---
+++ basics1/dateandtime.js
@@ -0,0 +1,46 @@
+// 
+
+let mydate =new Date()
+
+console.log(mydate);
+
+// console.log(mydate.toDateString());
+
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleString());
+
+// let mynewdate = new Date(2003,0,28,8)
+
+// let mynewdate = new Date("01-14-2024")
+// console.log(mynewdate.toLocaleString());
+// console.log(mynewdate.toLocaleString());
+
+
+let mytimestamp= Date.now()
+
+// console.log(mytimestamp);
+// console.log(Math.floor(mynewdate.getTime()/1000));
+
+let newdate = new Date()
+
+console.log(newdate.getDate());
+
+newdate.toLoacaleString('default',{
+    weekday: "long"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
