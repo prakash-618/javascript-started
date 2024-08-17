c908ba7030070bc294f3be66265810558e3d277b
prakash-618 <prakashranjan45890@gmail.com>
2024-08-17 08:00:19 +0000

    commit

---
+++ basics/numandmath.js
@@ -0,0 +1,44 @@
+// const score = 400
+
+
+// const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+// const othernum =23.8966
+
+// console.log(othernum.toPrecision(3));
+
+
+// const hun = 1000000
+// console.log(hun.toLocaleString('en-IN'));
+
+
+// ++++++++++++++ Maths ++++++++++++++++++
+
+
+// console.log(Math.abs(-4))
+
+// console.log(Math.round(4.6));
+
+// console.log(Math.sqrt(24).toPrecision(2));
+
+
+
+// console.log(Math.pow(2,5));
+
+
+console.log((Math.random()*10)+1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor((Math.random() * (max-min+1)))+min)
+
+
+
+
---
+++ basics/string.js
@@ -0,0 +1,16 @@
+const newstringone ="   hitesh   "
+console.log(newstringone)
+console.log(newstringone.trim())
+
+const url ="https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+console.log(url)
+
+console.log(url.includes('-'))
+
+const gamename = new String('prakash-ra-njanpa-ndey')
+
+console.log(gamename.split('-'))
+let name = "Prakash Ranjan Pandey"
+console.log(`Hello my name is ${name}`)
