4253db313114129b856af9fea9a9543a6e8906c4
prakash-618 <prakashranjan45890@gmail.com>
2024-08-18 06:18:01 +0000

    commit

---
+++ basics1/array.js
@@ -0,0 +1,83 @@
+// arrays
+
+// const myarr =[0,1,2,3,4,5,true,"hitesh"]
+
+// const myarr1 = new Array(1,2,3,4)
+
+// console.log(myarr[7]);
+// console.log(myarr1[3]);
+
+// methods of arrays
+
+// myarr.push(6)
+// myarr.pop()
+
+// myarr.unshift(9)
+
+
+// myarr.shift();
+// console.log(myarr);
+
+// const newarr =myarr.join()
+
+// console.log(typeof newarr);
+
+// console.log("A" , myarr);
+
+// const myn1 = myarr.slice(1,3)
+
+// console.log("B",myn1);
+// console.log("C",myarr);
+
+// const myn2 = myarr.splice(1,3)
+// console.log("D",myn2);
+// console.log("E",myarr);
+
+
+
+const num1 = [0,1,2]
+const num2 =[3,4,5]
+// num1.push(num2)
+
+const new1 = num1.concat(num2)
+
+// console.log(new1);
+
+
+// +++spread operator concatenate all elements into new array
+
+// const all = [...num1,...num2]
+
+// console.log(all)
+
+// ++++ flat convers array nestings into single array
+
+// const a =[1,2,[3,4,5],6]
+// const b = a.flat(Infinity)
+// console.log(a)
+// console.log(b)
+console.log(Array.from("Prakash"));
+
+let s1= 100;
+let s2=200;
+let s3 =300;
+
+console.log(Array.of(s1,s2,s3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
