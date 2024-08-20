7a98c0c920367ab77071a01b2994e4653c84bd06
prakash-618 <prakashranjan45890@gmail.com>
2024-08-20 15:28:26 +0000

    commit

--- basics1/functions.js
+++ basics1/functions.js
@@ -1,28 +1,44 @@
 
-function saymy(){
-    console.log("H")
-}
+// function saymy(){
+//     console.log("H")
+// }
 
-// saymy()
-function add(num1,num2){
-    let c=num1+num2
-    return c
-}
-let j = add(5,4)
-// console.log(j);
+// // saymy()
+// function add(num1,num2){
+//     let c=num1+num2
+//     return c
+// }
+// let j = add(5,4)
+// // console.log(j);
 
-function loginuser(username){
-    if(username){
-        return `${username} just logged in`
-    }
-    else{
-        console.log("not provided credentials")
+// function loginuser(username){
+//     if(username){
+//         return `${username} just logged in`
+//     }
+//     else{
+//         console.log("not provided credentials")
 
-    }
+//     }
     
+// }
+// console.log(loginuser())
+
+
+// rest operator is here but also called spread operator
+
+function add(...num1){
+    return num1
 }
-console.log(loginuser())
+// console.log(add(2,4,6,100))
 
+const user ={
+    name:"Prakash",
+    price:199
+}
 
+function handleuser(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`)
+}
 
+handleuser(user)
 
