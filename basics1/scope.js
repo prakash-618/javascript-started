d50a13e620d7d9336761d0f84cbd099ba1f1e9a6
prakash-618 <prakashranjan45890@gmail.com>
2024-08-22 10:38:21 +0000

    commit

--- basics1/scope.js
+++ basics1/scope.js
@@ -12,3 +12,43 @@ if(true){
 // a that is defined outside the if condition is global scope
 // but inside if condition is block scope
 
+
+function one(){
+    const username = "Prakash"
+    function two(){
+        const website ="Youtube"
+        console.log(username)
+    }
+
+    // console.log(website)
+    two()
+}
+// one()
+
+if(true){
+    const username ="Prakash"
+    if(username==="Prakash"){
+        const website =" youtube"
+        // console.log(username+website);  
+    }
+
+    // console.log(website)
+}
+// console.log(username)
+
+// ++++++++++++++   Interesting +++++++++++++++
+// this type of calling can be done 
+// const j =addone(5)
+// console.log(j);
+function addone(value){
+    return value+1
+}
+
+// this type of calling cant be done
+addtwo(5)
+const addtwo=function(num){
+    return num;
+}
+
+
+
