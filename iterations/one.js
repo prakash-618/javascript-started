92e258c0193bd24007e5d4cef6573430eb36e758
prakash-618 <prakashranjan45890@gmail.com>
2024-08-25 07:15:45 +0000

    loop

---
+++ iterations/one.js
@@ -0,0 +1,16 @@
+// console.log("Rachit")
+
+for (let i = 24; i <25; i++) {
+    for(let j =1 ;j<=10 ;j++){
+        // console.log(i+'*'+j+'='+i*j)
+    }
+}
+
+// break and continue
+
+for(let i=1;i<=20;i++){
+    if(i==5){
+        console.log(`Value of i is ${i}`)
+    }
+   
+}
